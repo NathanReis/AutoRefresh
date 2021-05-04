c7259dd5f2da7e69dbd443f52f0728f8eca3e379
Nathan Reis <53984490+NathanReis@users.noreply.github.com>
2021-05-04 17:34:42 -0300

    Melhoria do repositorio

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# AutoRefresh
+# Como usar
+
+Existem 3 formas de uso:
+
+1. Informar apenas período
+
+É útil quando for necessário um refresh em períodos de tempo fixo.
+
+2. Informar apenas início
+
+É útil quando a partir de um momento o refresh tem q acontecer imediatamente.
+
+Desta forma deve ser informado um período de 0.
+
+3. Informar período e início
+
+É útil quando o refresh tiver que acontecer somente depois que um determinado momento tiver passado.
+
+Se comprar com a forma 2, pode-se dizer que informar um período é adicionar um atraso no refresh.
+
+---
+
+Um uso que pode ser útil é fazer um refresh diário, para este caso, o valor do início deve ser uma string no formato HH:mm.
--- js/autoRefresh.js
+++ js/autoRefresh.js
@@ -1,56 +1,86 @@
+document
+	.querySelector("time")
+	.innerHTML = (new Date()).toLocaleString("pt-br");
+
 setTimeout(function() {
 	location.href = location.href;
-}, calcularTempoRestante());
+}, calcTimeLeftToRefresh(5000));
 
-function calcularTempoRestante() {
-	var dateObj = new Date();
+/**
+ * Calcula quantos milesegundos faltam através de um valor para período e um momento de início
+ * 
+ * @param {number} period 
+ * @param {Date|string} start 
+ * 
+ * @returns {number}
+ */
+function calcTimeLeftToRefresh(period, start) {
+	if (!start) {
+		/**
+		 * Quando não receber um momento inicial
+		 * Esperar apenas o próprio período
+		 */
 
-	// Configurado para 24:00:00.000
-	var horaAlvo_s = 24 * 60 * 60;
-	var minutoAlvo_s = 0 * 60;
-	var segundoAlvo_s = 0;
-	var milisegundoAlvo_ms = 0; 
-
-	// Soma todos segundos alvos
-	// Converte para milisegundos
-	// Soma com os milisegundos alvo
-	var totalAlvo_ms = (horaAlvo_s + minutoAlvo_s + segundoAlvo_s) * 1000 + milisegundoAlvo_ms;
-
-	// Configurado para hh:mm:ss.fff
-	// hh -> Hora
-	// mm -> Minuto
-	// ss -> Segundo
-	// fff -> Acho que representa Milisegundo
-	var horaAtual_s = dateObj.getHours() * 60 * 60;
-	var minutoAtual_s = dateObj.getMinutes() * 60;
-	var segundoAtual_s = dateObj.getSeconds();
-	var milisegundoAtual_ms = dateObj.getMilliseconds();
-
-	// Soma todos segundos atuais
-	// Converte para milisegundos
-	// Soma com os milisegundos atuais
-	var totalAtual_ms = (horaAtual_s + minutoAtual_s + segundoAtual_s) * 1000 + milisegundoAtual_ms;
-
-	// A diferença entre alvo e atual resulta no tempo restante para o tempo alvo
-	var tempoRestante_ms = totalAlvo_ms - totalAtual_ms;
-
-	if(tempoRestante_ms < 0) {
-		// Caso deferença seja negativa
-		// Significa que falta menos de um dia para atualizar
-		// Então é calculado a quantidade de milisegundos em um dia
-		// Somado, pois o valor é negativo, com o tempo restante
-		// Exemplo:
-		// ---------
-		// Set 08:25
-		// ---------
-		// Current 08:24
-		// Dif 1min
-		// Current 08:25
-		// Dif 0min
-		// Current 08:26
-		// Dif -1min = 23:59
-		tempoRestante_ms = (24 * 60 * 60 * 1000) + tempoRestante_ms;
+		return period;
 	}
-	
-	return tempoRestante_ms;
+
+	const NOW = new Date();
+
+	if (typeof start === "string") {
+		/**
+		 * Quando não recebido um objeto
+		 * Verificar se a string possuí um valor válido
+		 */
+
+		const REGEX_DATE = /^\d{4}-\d{2}-\d{2}$/;
+		const REGEX_TIME = /^\d{2}:\d{2}$/;
+		const REGEX_DATE_TIME = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}$/;
+
+		if (REGEX_DATE.test(start) || REGEX_DATE_TIME.test(start)) {
+			/**
+			 * Quando a string possuir um formato valído contendo a data
+			 * Utilizar a própia string para criar o objeto
+			 */
+
+			start = new Date(start);
+		} else if (REGEX_TIME.test(start)) {
+			/**
+			 * Quando a string possuir um formato valído contendo apenas a hora
+			 * Utilizar a data de hoje junto a hora informada para montar o objeto
+			 */
+
+			let dateNow = String(NOW.getDate()).padStart(2, "0");
+			let monthNow = String(NOW.getMonth() + 1).padStart(2, "0");
+			let fullYearNow = NOW.getFullYear();
+
+			start = new Date(`${fullYearNow}-${monthNow}-${dateNow}T${start}`);
+
+			if (NOW.getTime() > start.getTime()) {
+				/**
+				 * Quando a data de início já tiver passado
+				 * Somar 1 dia para considerar a contagem a partir do dia seguinte
+				 */
+
+				start.setDate(start.getDate() + 1);
+			}
+		} else {
+			throw new Error("Valor inválido de início");
+		}
+	}
+
+	if (NOW.getTime() > start.getTime()) {
+		/**
+		 * Quando já passou o momento do refresh
+		 * Retornar 0 pois não tem que esperar nada
+		 */
+
+		return 0;
+	}
+
+	/**
+	 * É subtraido do momento esperado o agora e o período para esperar
+	 */
+	let timeLeft = start.getTime() - (NOW.getTime() + period);
+
+	return timeLeft;
 }
